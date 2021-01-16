6c4e8418c03a31eb25d56dfafb3f03221a6ecd6c
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2021-01-16 17:30:35 -0300

    fix user

--- frontend/src/providers/logged-user.provider.js
+++ frontend/src/providers/logged-user.provider.js
@@ -2,6 +2,6 @@ import { safeJWTDecode } from 'app-helpers'
 import createGlobalState from 'react-create-global-state'
 
 const token = localStorage.getItem('sd-logged-user')
-const { user } = safeJWTDecode(token)
+const { user } = safeJWTDecode(token) || {}
 
 export const [useGlobalLoggedUser, LoggedUserProvider] = createGlobalState(user)
