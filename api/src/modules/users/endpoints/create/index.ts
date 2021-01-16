af8a61b4d835a2b94530300a60be1df1f6ddf699
Luis Miguel Pires <luismiguelprs@gmail.com>
2021-01-16 18:10:22 -0300

    profileDone no token

--- api/src/modules/users/endpoints/create/index.ts
+++ api/src/modules/users/endpoints/create/index.ts
@@ -1,13 +1,21 @@
-import { database } from '../../../../datasources/knex';
-import { UsersRepository } from '../../users.repository';
-import { UserLoginService } from '../login/user-login.service';
-import { UserCreateController } from './user-create.controller';
-import { UserCreateService } from './user-create.service';
+import { database } from '../../../../datasources/knex'
+import { CompanyProfileRepository } from '../../../company-profile/company-profile.repository'
+import { PersonProfileRepository } from '../../../person-profile/person-profile.repository'
+import { UsersRepository } from '../../users.repository'
+import { UserLoginService } from '../login/user-login.service'
+import { UserCreateController } from './user-create.controller'
+import { UserCreateService } from './user-create.service'
 
-const usersRepository = new UsersRepository(database);
+const usersRepository = new UsersRepository(database)
+const personProfileRepository = new PersonProfileRepository(database)
+const companyProfileRepository = new CompanyProfileRepository(database)
 
-const userCreate = new UserCreateService(usersRepository);
-const userLogin = new UserLoginService(usersRepository);
-const userCreateController = new UserCreateController(userCreate, userLogin);
+const userCreate = new UserCreateService(usersRepository)
+const userLogin = new UserLoginService(
+  usersRepository,
+  personProfileRepository,
+  companyProfileRepository
+)
+const userCreateController = new UserCreateController(userCreate, userLogin)
 
-export { userCreateController };
+export { userCreateController }
