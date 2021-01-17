3696e1ab46a908b8b0c3db32b385c98372462179
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2021-01-16 22:37:55 -0300

    b

--- frontend/src/ui/pages/home/home.page.jsx
+++ frontend/src/ui/pages/home/home.page.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createRoute } from 'app-route-manager'
 import './home.style.scss'
+import { useGlobalLoggedUser } from 'app-providers'
+import { useRoute } from 'app-hooks'
 
 export const Home = () => {
+  const [loggedUser] = useGlobalLoggedUser()
+  const { goToUpdateProfile } = useRoute()
+
+  useEffect(() => {
+    if (!loggedUser.profileDone) {
+      goToUpdateProfile()
+    }
+  }, [])
+
   return (
     <div className="home">
       <h1>home</h1>
